a008a6b42f7799df2389f246481bc51fb76a472a
Ayoade1st <fissyayoade@gmail.com>
2023-12-13 03:02:08 -0800

    updated

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Ayoade1st/alx-low_level_programming/raw/main/0x18-dynamic_libraries/iwin.so
-export LD_PRELOAD=/tmp/iwin.so
+wget -P .. https://raw.githubusercontent.com/Matsadura/alx-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD="$PWD/../libgiga.so"
